e448a589b28515684171b55edd07a6eae52915dd
unknown <ashsquads@gmail.com>
2020-08-07 13:25:44 -0500

    Locked Screen Orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 import 'package:smash_tracker/pages/home.dart';
 import 'package:smash_tracker/pages/player_card.dart';
 import 'package:smash_tracker/services/add_dropdown_service.dart';
 
-void main() {
-  runApp(MaterialApp(
-    initialRoute: '/home',
-    routes: {
-      '/home': (context) => Home(),
-      '/playerCard': (context) => PlayerCard(),
-      '/addPlayer': (context) => AddPlayer(),
-    },
-  ));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+    runApp(MaterialApp(
+      initialRoute: '/home',
+      routes: {
+        '/home': (context) => Home(),
+        '/playerCard': (context) => PlayerCard(),
+        '/addPlayer': (context) => AddPlayer(),
+      },
+    ));
 }
 
 
